38a60056d41b2d90bb3abb9262e5cb894a36d196
khushi02410 <khushirajpurohit2211@gmail.com>
2025-08-12 15:58:46 +0530

    29562

--- restaurant_management/views.py
+++ restaurant_management/views.py
@@ -5,3 +5,13 @@ def home(request):
     return render (request , 'home.html',{
         'restaurent_name':settings.RESTAURENT_NAME
     })
+
+def menu_list(request):
+    menu_items = [
+    {"name": "margherita jambo pizza", "prize":500},
+    {"name": "penne pasta", "price:400"},
+    {"name": "veg burger", "price:200"},
+    {"name": "chocolate brownie" ,"price:340"}
+    ]
+    return render(request,"menu.html",{"menu_items":menu_items})
+    
