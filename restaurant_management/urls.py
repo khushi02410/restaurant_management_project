c2044577235d562ecaf7033dd628608291103908
khushi02410 <khushirajpurohit2211@gmail.com>
2025-08-12 18:17:21 +0530

    29566

--- restaurant_management/urls.py
+++ restaurant_management/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('api/products/',include('products.urls')),
     path('api/orders/',include('orders.urls')),
     path('', include('home.urls')),
+    Path('',views.home,name='home'),
     
 ]
