d9056e324a52f70499163c246ec9cf3dbdedc684
khushi02410 <khushirajpurohit2211@gmail.com>
2025-08-13 14:11:28 +0530

    31767

--- home/views.py
+++ home/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from datetime import datetime
 # Create your views here.
 
 
@@ -13,3 +13,19 @@ def home(request):
     {
         'restaurant_name':'KIKIS KITECHEN'
     })  
+
+def home(request):
+    return render(request, 'home/home.html',{'current_year': datetime.now().year})
+
+def about(request):
+    return render(request, 'home/about.html',{'current_year': datetime.now().year})
+
+def contact(request):
+    return render(request,'home/contact.html':{'current_year': datetime.now().year})
+
+def menu(request):
+    return render(request,'home/menu.hmtl':{'current_year': datetime.now().year})
+
+def reservations(request):
+    return render(request,'home/reservations.html':{'current_year': datetime.now().year})
+
