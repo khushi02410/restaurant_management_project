b5f4efb9c303a740ccc1a1fddc12fd70559e88fe
khushi02410 <khushirajpurohit2211@gmail.com>
2025-08-18 10:59:37 +0530

    34971

--- home/models.py
+++ home/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User 
 
 class userProfile(models.Model):
-    user = models.oneToFeild, on_delete = models.CASADE , related= "profile)
+    user = models.oneToFeild, on_delete = models.CASADE , related_name= "profile)
     phone_number = models.CharFeild(max_length=15 , black = true , nul = True)
 
     def __str__(self):
