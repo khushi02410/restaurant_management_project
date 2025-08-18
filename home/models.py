bdac5798cb911a05c6fdef58f29e03e72f9b3aea
khushi02410 <khushirajpurohit2211@gmail.com>
2025-08-18 10:57:10 +0530

    34971

--- home/models.py
+++ home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User 
 
-# Create your models here.
+class userProfile(models.Model):
+    user = models.oneToFeild, on_delete = models.CASADE , related= "profile)
+    phone_number = models.CharFeild(max_length=15 , black = true , nul = True)
+
+    def __str__(self):
+        retuen self.user.username
