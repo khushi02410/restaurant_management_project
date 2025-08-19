a26aa6e9cebace91f2c2abaa9631629b80324da4
khushi02410 <khushirajpurohit2211@gmail.com>
2025-08-19 12:26:57 +0530

    34973

--- orders/models.py
+++ orders/models.py
@@ -17,5 +17,5 @@ class Order(models.Model):
         status = models.CharField(max_length = 20 , choices = STATUS_CHOICES , default = 'pending')
         created_at = models.DateTimeField(auto_now_add = True)
 
-def __str__ (self):
-    return f"Order {self.id} by {self.customer.username}"
+    def __str__(self):
+        return f"Order {self.id} by {self.customer.username}"
