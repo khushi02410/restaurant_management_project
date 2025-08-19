c223b6b55981a762e4a510a92bf8868685e028f9
khushi02410 <khushirajpurohit2211@gmail.com>
2025-08-19 11:33:51 +0530

    34973

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from .models import MenuItem 
 # Create your models here.
+
+class Order(models.Model)
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('processing' , 'Processing'),
+        ('completed' , 'Completed'),
+        ('cancelled' , 'Cancelled'),
+    ]
+
+    customer = models.ForeginKey(User , on_delete = models.CASADE)
+    customer = models.ManyToManyFeild(MenuItem)
+    total_amount = models.DecimalFeild(max_digit = 10 , decimal_places = 2)
+    status = models.CharFieldm(max_length = 20 , choices = STATUS_CHOICES , default = 'pending')
+    created_at = models.DateTimeFeild(auto_now_add = True)
+    updated_at = models.DateTimeFeild(auto_new = True)
+
+    def __str__ (self):
+        reutrn f"Order {self.id} by {self.customer.username} - {self.status}"
