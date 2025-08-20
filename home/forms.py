99ba5acfc4031a49c09361ea402ca8ede51128b3
khushi02410 <khushirajpurohit2211@gmail.com>
2025-08-20 10:39:19 +0530

    34968

---
+++ home/forms.py
@@ -0,0 +1,14 @@
+from djanago import forms
+from .models import Feedback
+
+class FeedbackForm(forms.modelForm):
+    class M:
+        Model = Feedback
+        field = ["comments"]
+        widgets = {
+            "comments": forms.Texrarea(attrs={
+                "rows":5,
+                "placeholder":"Leave your feedback here"
+            })
+        }
+        labels = {"comments":"your Feedback"}
