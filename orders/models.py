b147dbb864375f40cbe1239298d6d630820d4a6a
khushi02410 <khushirajpurohit2211@gmail.com>
2025-08-20 14:28:13 +0530

    34974

--- orders/models.py
+++ orders/models.py
@@ -3,19 +3,25 @@ from django.contrib.auth.models import User
 from restaurant.models import MenuItem 
 # Create your models here.
 
-class Order(models.Model):
-    STATUS_CHOICES = [
-        ('pending','Pending'),
-        ('processing','Processing'),
-        ('completed','Completed'),
-        ('cancelled','Cancelled'),
-    ]
-
-        customer = models.ForeignKey(User , on_delete=models.CASCADE)
-        order_items = models.ManyToManyField(MenuItem)
-        total_amount = models.DecimalField(max_digits = 10 , decimal_places = 2)
-        status = models.CharField(max_length = 20 , choices = STATUS_CHOICES , default = 'pending')
-        created_at = models.DateTimeField(auto_now_add = True)
+class Menu(models.model):
+    name= models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6,decimal_places=2)
 
     def __str__(self):
-        return f"Order {self.id} by {self.customer.username}"
+        return self.name
+
+
+
+class Order(models.Model):
+ 
+    customer = models.ForeignKey(User , on_delete=models.CASCADE)
+    order_items = models.ManyToManyField(Menu)
+    total_amount = models.DecimalField(max_digits = 10 , decimal_places = 2)
+    status = models.CharField(max_length = 20 , choices = [("pending","Pending"),("comoleted","Completed"),("cancelled","Cancelled")],default="pending"
+    )
+    # created_at = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return f"Order #{self.id} by {self.customer.username}"
+ 
