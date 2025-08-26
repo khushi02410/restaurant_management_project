83fead0a9c08318e95c55176594392acc1e4e0d8
khushi02410 <khushirajpurohit2211@gmail.com>
2025-08-26 11:31:56 +0530

    38870

--- orders/admin.py
+++ orders/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Menu , Order 
+from .models import MenuItem
+
 # Register your models here.
+admin.site.register(MenuItem)
 
 class MenuAdmin(admin.ModelAdmin):
     list_display = ("name","price","description")
