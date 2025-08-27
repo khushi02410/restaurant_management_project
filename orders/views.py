4f4f92f1305fb503e3b7cbe5c3e2236d2389d70c
khushi02410 <khushirajpurohit2211@gmail.com>
2025-08-27 19:12:55 +0530

    38871

--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import MenuItem
 
 # Create your views here.
 path('/orders',OrdersListView.as_view(),name='orders'),
 
+def menu_view(request):
+    item = MenuItem.objects.all()
+    return render(request, 'menu.html', {'items': items})
+    
