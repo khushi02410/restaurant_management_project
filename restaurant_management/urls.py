9f26b28feae20699e3165cb83291752679c70f56
khushi02410 <khushirajpurohit2211@gmail.com>
2025-09-02 00:11:58 +0530

    40432

--- restaurant_management/urls.py
+++ restaurant_management/urls.py
@@ -36,3 +36,6 @@ urlpatterns = [
     path("api/",include("restaurant.url")),
     path('', include('order.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL , document_root=settings.MEDIA_ROOT)
