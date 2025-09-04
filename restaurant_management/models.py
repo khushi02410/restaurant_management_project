cb6b348ccc2a0c29e710da6685b2e3f772bef3e0
khushi02410 <khushirajpurohit2211@gmail.com>
2025-09-04 15:09:06 +0530

    42100

--- restaurant_management/models.py
+++ restaurant_management/models.py
@@ -1,9 +1,16 @@
-from RestaurantLocation(models.Model):
+from django.db import models
+
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length= 200)
+    street = models.CharField(max_length=255)
     address = models.CharField(max_length = 252)
     city = models.CharField(max_length = 100)
     state  = models.CharField(max_length = 100)
     zip_code = models.CharField(max_length = 20)
+    opening_hours = models.JSONField(default = dict)
 
     def __str__(self):
-        return f"{self.address}, {self.city}, {self.state} - {self.zip_code}"
+        return f"{self.address}, {self.name}, {self.city}, {self.state} - {self.zip_code}"
+        
         
