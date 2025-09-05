9ceaf623ccf367f91be6455cd6208966b32be778
khushi02410 <khushirajpurohit2211@gmail.com>
2025-09-05 23:28:02 +0530

    42099

--- orders/models.py
+++ orders/models.py
@@ -39,7 +39,7 @@ class Order(models.Model):
 class MenuItem(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank = True , null = True)
-    price = models.DecimalField(max_digits = 8 , decimal_places = 2) 
+    price = models.DecimalField(max_digits = 6 , decimal_places = 2) 
     image = models.ImageField(upload_to ='menu_images/', blank = True , null=True )
 
     def __str__(self):
