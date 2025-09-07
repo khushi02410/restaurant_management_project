104408c30861b550c0f77869f1d10855e2212214
khushi02410 <khushirajpurohit2211@gmail.com>
2025-09-07 18:45:53 +0530

    42097

--- orders/views.py
+++ orders/views.py
@@ -9,5 +9,11 @@ def menu_view(request):
     return render(request, 'menu.html', {'items': items})
     
 def home_view(request):
-    featured_items = MenuItem.objects.all()[:3]
-    return render(request, 'home.html' , {'featured_items': featured_items})
+    query = request.GET.get("q")
+    if query:
+        menu_items = MenuItem.objects.filter(name__icontaind=query)
+    else:
+        menu_items = MenuItem.objects.all()
+
+    # featured_items = MenuItem.objects.all()[:3]
+    return render(request, 'home.html' , {'menu_items': menu_items, "query": query})
