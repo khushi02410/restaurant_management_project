1bb59fcea50f11a804cab8062ef71253b6cc889e
khushi02410 <khushirajpurohit2211@gmail.com>
2025-09-07 17:52:30 +0530

    42098

--- home/models.py
+++ home/models.py
@@ -18,9 +18,11 @@ class OpeningHours(models.Model):
 class Contact(models.Model):
     name = models.CharFeild(max_length = 100)
     email = models.EmailField()
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add = True)
 
     def __str__(self):
-        return self.name
+        return f"{self.name} - {self.email}"
 
 class RestaurantAddress(models.Model):
     address = models.TextField()
