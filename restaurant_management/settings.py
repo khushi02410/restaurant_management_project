cc426f06dba83423b0050330c02a993b0b025c84
khushi02410 <khushirajpurohit2211@gmail.com>
2025-09-07 17:47:06 +0530

    42098

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -136,3 +136,5 @@ RESTAURANT_ADDRESS = "Near gls dominos pizza"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR , 'media')
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
