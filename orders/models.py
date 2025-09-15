0428bcfbb4b02dc607255e717f1c04e55ecaff1f
khushi02410 <khushirajpurohit2211@gmail.com>
2025-09-15 11:01:56 +0530

    44683

--- orders/models.py
+++ orders/models.py
@@ -13,6 +13,12 @@ class Menu(models.model):
         return self.name
 
 
+class OrderStatus(models.Model):
+    name = models.CharField(max_length = 40)
+
+    def __str__(self):
+        return self.name
+
 
 class Order(models.Model):
  
@@ -21,7 +27,7 @@ class Order(models.Model):
     order_items = models.ManyToManyField(Menu)
     order_date = models.DateTimeField(auto_now_add = True)
     total_amount = models.DecimalField(max_digits = 10 , decimal_places = 2)
-    status = models.CharField(max_length = 20 , choices = [("pending","Pending"),("comoleted","Completed"),("cancelled","Cancelled")],default="pending"
+    status = models.CharField(max_length = 20 , choices = [("pending","Pending"),("completed","Completed"),("cancelled","Cancelled")],default="pending"
     )
     # created_at = models.DateTimeField(auto_now_add = True)
 
@@ -31,10 +37,19 @@ class Order(models.Model):
         related_name = "orders"
     )
 
+    status = models.ForeignKey(
+        OrderStatus,
+        on_delete = models.SET_NULL,
+        null = True,
+        blank = True
+    )
+
     def __str__(self):
-        return f"Order #{self.id} by {self.customer.username}"
+        return f"Order #{self.id} by {self.customer.name}"
         return f"Order by {self.customer_name} at {self.restaurant.city} on {self.order_date}"
- 
+
+
+
 
 class MenuItem(models.Model):
     name = models.CharField(max_length=100)
