17e7d5e50aaae7926fa81a12c82c5c0f05bae598
khushi02410 <khushirajpurohit2211@gmail.com>
2025-09-15 11:17:44 +0530

    44682

--- orders/models.py
+++ orders/models.py
@@ -14,7 +14,7 @@ class Menu(models.model):
 
 
 class OrderStatus(models.Model):
-    name = models.CharField(max_length = 40)
+    name = models.CharField(max_length = 40 , unique = True)
 
     def __str__(self):
         return self.name
