c277cf73df4590e1bdd46fd8b4aff9671b1454af
khushi02410 <khushirajpurohit2211@gmail.com>
2025-09-15 11:31:27 +0530

    44679

--- home/models.py
+++ home/models.py
@@ -2,21 +2,21 @@ from django.db import models
 from django.contrib.auth.models import User 
 
 class userProfile(models.Model):
-    user = models.oneToFeild, on_delete = models.CASADE , related_name= "profile)
-    phone_number = models.CharFeild(max_length=15 , black = true , nul = True)
+    user = models.oneToField, on_delete = models.CASADE , related_name= "profile)
+    phone_number = models.CharField(max_length=15 , blank = True , null=True)
 
     def __str__(self):
-        retuen self.user.username
+        return self.user.username
 
 class OpeningHours(models.Model):
-    days = model.CharFeild(max_length=100)
-    hours = models.CharFeild(max_length=100)
+    days = model.CharField(max_length=100)
+    hours = models.CharField(max_length=100)
 
     def __str__(self):
         return f"{self.days}: {self.hours}"
 
 class Contact(models.Model):
-    name = models.CharFeild(max_length = 100)
+    name = models.CharField(max_length = 100)
     email = models.EmailField()
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add = True)
@@ -26,16 +26,16 @@ class Contact(models.Model):
 
 class RestaurantAddress(models.Model):
     address = models.TextField()
-    city = models.CharFeild(max_length=100)
-    state = models.CharFeild(max_length = 100 , blank = True , null = True)
-    zipcode = models.CharFeild(max_length=20, blank = True ,null = True)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length = 100 , blank=True , null=True)
+    zipcode = models.CharField(max_length=20, blank=True ,null=True)
 
     def __str__(self):
         return f"{self.address}, {self.city}"
 
 
 class MenuCategory(models.Model):
-    name = models.CharFeild(max_length = 100 , unique = True)
+    name = models.CharField(max_length = 100 , unique = True)
 
     def __str__(self):
         return self.name        
