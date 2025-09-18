9159a16dc0b38486a242569796761b116d6a1578
khushi02410 <khushirajpurohit2211@gmail.com>
2025-09-18 18:04:43 +0530

    45914

--- orders/serializers.py
+++ orders/serializers.py
@@ -5,4 +5,11 @@ class OrderItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = OrderItem
         fields = ['id' , 'menu_item' , 'quantity', 'price']
-        
+
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True , read_only = True , source = "orderitem_set")
+
+    class Meta:
+        model = Order
+        fields = ['id','date','total_price','items']
+                
