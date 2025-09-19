31ccbd98c2cd04fba5786eaf0c7abecee0fc1c00
khushi02410 <khushirajpurohit2211@gmail.com>
2025-09-19 11:40:52 +0530

    45913

--- home/serializers.py
+++ home/serializers.py
@@ -5,3 +5,10 @@ class MenuCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = MenuCategory
         fields = ['id','name']
+
+class MenuItemSerializer(serializers.ModelSerializer):
+    category = serializers.StringRelatedFeild()
+
+    class Meta:
+        model=MenuItem
+        feilds = ['id' , 'name']
