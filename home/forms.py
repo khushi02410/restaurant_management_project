0404fb46aab1077563ea58f032fbe1495d879723
khushi02410 <khushirajpurohit2211@gmail.com>
2025-09-21 13:28:05 +0530

    45912

--- home/forms.py
+++ home/forms.py
@@ -1,6 +1,7 @@
 from djanago import forms
 from .models import Feedback
 from .models import Contact
+from utils.validation_utils import is_valid_email
 
 class FeedbackForm(forms.modelForm):
     class M:
@@ -18,3 +19,9 @@ class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
         fields = ["name","email", 'message']
+
+class SignupForm(forms.Form):
+    email = self.cleaned_data.get("email")
+    if not is_valid_email(email):
+        raise forms.validateionError("please enter a valid email id address")
+    return email        
