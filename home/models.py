8380ae9f8615f94bd93b7597e3c3b3c6f467a40f
khushi02410 <khushirajpurohit2211@gmail.com>
2025-09-21 13:43:56 +0530

    45911

--- home/models.py
+++ home/models.py
@@ -45,7 +45,7 @@ class MenuItem(models.Model):
     description = models.TextField(blank=True,null=True)
     price = models.DecimalField(max_digit=5, decimal_places=2)
     category = models.ForeignKey(MenuCategory,on_delete=models.CASCADE, related_name = "items")
-
+    available = models.BooleanFeild(default=True)
     
     def __str__(self):
         return self.name
