76badb27217ee200e128a1ff21c0d31815c41d3a
khushi02410 <khushirajpurohit2211@gmail.com>
2025-09-24 17:17:08 +0530

    50116

--- orders/urls.py
+++ orders/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from .views import *
 from .views import menu_view
 from .views import OrderHistoryView
+from .views import OrderDetailView
 
 urlpatterns = [
     path('menu/' , menu_view , name='menu'),
     path('order-history/', OrderHistoryView.as_view(), name='order-history'),
+    path("order/<int:id>/" , OrderDetailView.as_view() , name="order-detail"),
 ]
