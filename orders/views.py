bbe59dee4af3f3f822789754aaac8b9cfeaf1f00
khushi02410 <khushirajpurohit2211@gmail.com>
2025-10-02 16:16:55 +0530

    52762

--- orders/views.py
+++ orders/views.py
@@ -4,7 +4,14 @@ from .models import Order
 from .serializers import OrderSerializer
 from .utils  import send_order_confirmation_email
 from rest_framework.generics import RetriveAPIView
-from rest_framework].permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.utils import timezone
+from .models import coupon
+from .serializers import CouponSerializer
+
 
 # Create your views here.
 path('/orders',OrdersListView.as_view(),name='orders'),
@@ -35,3 +42,40 @@ class OrderDetailView(RetriveAPIView):
     serializer_class = OrderSerializer
     permission_classes = [IsAuthenticated]
     lookup_field = "id"        
+
+class CouponValidationView(APIView):
+    def post(self , request , *args , **kwargs):
+        code = request.data.get("code" , "").strip()
+
+        if node code:
+            return Response(
+                {"error":"Coupon code is required"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        try:
+            coupon = Coupon.objects.get(code__iexact=code)
+        except Coupon.DoesNotExist:
+            return Response(
+                {"error":"Invalid coupon code"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        today = timezone.now().date()
+        if not coupon.is_active:
+            return Response(
+                {"error": "This coupon is not ative"},
+                status = status.HTTP_400_BAD_REQUEST
+            )
+        if today < coupon.valid_from or today > coupon.valid_until:
+            return Response(
+                {"error":"This coupon is not valid at this point "},
+                status = status.HTTP_400_BAD_REQUEST
+            )
+
+        serializer = CouponSerializer(coupon )
+        return Response(
+            {"success": True , "coupon":serializer.data},
+            staus =status.HTTP_200_OK
+        )
+
