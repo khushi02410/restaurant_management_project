6f059e12fdceeba5651820ea858b5c7c28f8d8de
khushi02410 <khushirajpurohit2211@gmail.com>
2025-10-02 16:19:04 +0530

    52762

--- orders/urls.py
+++ orders/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('menu/' , menu_view , name='menu'),
     path('order-history/', OrderHistoryView.as_view(), name='order-history'),
     path("order/<int:id>/" , OrderDetailView.as_view() , name="order-detail"),
+    path("coupon/validate/" , CouponValidationView.as_view(), name='coupon-validate'),
 ]
