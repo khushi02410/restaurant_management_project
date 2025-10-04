3966cad92818512d743aceaacb352a7701bf8a25
wdcs-HimanshuPurohit <himanshu.purohit@webcluesinfotech.com>
2025-10-04 17:42:02 +0530

    updated utils file

---
+++ home/utils.py
@@ -0,0 +1,25 @@
+from datetime import datetime, time
+
+def is_restaurant_open():
+    now = datetime.now()
+    current_day = now.strftime("%A") 
+    current_time = now.time()
+
+    # Example opening hours (you can customize these)
+    # Each day maps to (opening_time, closing_time)
+    opening_hours = {
+        "Monday": (time(9, 0), time(22, 0)),     # 9 AM - 10 PM
+        "Tuesday": (time(9, 0), time(22, 0)),
+        "Wednesday": (time(9, 0), time(22, 0)),
+        "Thursday": (time(9, 0), time(22, 0)),
+        "Friday": (time(9, 0), time(23, 0)),     # Open till 11 PM on Fridays
+        "Saturday": (time(10, 0), time(23, 0)),  # Open 10 AM - 11 PM
+        "Sunday": (time(10, 0), time(21, 0)),    # Open 10 AM - 9 PM
+    }
+
+    if current_day not in opening_hours:
+        return False  # closed if no data available
+
+    open_time, close_time = opening_hours[current_day]
+
+    return open_time <= current_time <= close_time
